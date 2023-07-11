f23484b29ffcc335f0485dc5697a69477eb598a1
Aaron <iaaron.xyz@outlook.com>
2023-07-11 14:45:37 -0600

    display a number of emojis based on difficulty

--- src/components/MainGame/Board.js
+++ src/components/MainGame/Board.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import emojis from '../../data/emojis';
 
 const Board = ({numCards}) => {
+
+  const [cardsInGame, setCardsInGame] = useState([]);
+  
+  useEffect(() => {
+    const selectCardSymbols = () => {
+      const selectedSymbols = [];
+      // const indices = [];
+
+      for (let symbol = 0; symbol < numCards; symbol += 1) {
+        selectedSymbols.push(emojis[symbol]);
+      }
+
+      setCardsInGame(selectedSymbols);
+    }
+
+    selectCardSymbols();
+  }, [numCards])
+
   return (
-    <div className='grow flex justify-center items-center text-6xl'>
-      Num cards: {numCards}
+    <div className='grow flex flex-col justify-center items-center text-8xl bg-slate-600'>
+      {/* Num cards: {numCards} */}
+      <div className='cards-container flex justify-center items-center flex-wrap'>
+        {cardsInGame.map((cardChar) => {
+          console.log(cardChar);
+          return (
+            <div key={cardChar.code} className='justify-center items-center bg-white p-6 m-4'>
+              {cardChar.emoji}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
---
+++ src/data/emojis.js
@@ -0,0 +1,54 @@
+const emojisCompact = [
+  {
+    emoji: "🐵",
+    name: "Monkey Face",
+    code: "128053",
+  },
+  {
+    emoji: "🐒",
+    name: "Monkey",
+    code: "128018",
+  },
+  {
+    emoji: "🦍",
+    name: "Gorilla",
+    code: "129421",
+  },
+  {
+    emoji: "🐶",
+    name: "Dog Face",
+    code: "128054",
+  },
+  {
+    emoji: "🐺",
+    name: "Wolf Face",
+    code: "128058",
+  },
+  {
+    emoji: "🦊",
+    name: "Fox Face",
+    code: "129418",
+  },
+  {
+    emoji: "🦝",
+    name: "Racoon Face",
+    code: "129437",
+  },
+  {
+    emoji: "🦄",
+    name: "Unicorn Face",
+    code: "129412",
+  },
+  {
+    emoji: "🐣",
+    name: "Hatching Chick",
+    code: "128035",
+  },
+  {
+    emoji: "🐋",
+    name: "Whale",
+    code: "128011",
+  },
+]
+
+export default emojisCompact;
