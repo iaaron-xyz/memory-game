ebda080c62c005f5a2d46188d63eb751eb32fc99
Aaron <iaaron.xyz@outlook.com>
2023-07-18 02:54:36 -0600

    animate question-mark button and message

--- src/components/MainGame/BottomElement.js
+++ src/components/MainGame/BottomElement.js
@@ -1,8 +1,21 @@
+import { useState } from "react"
 
 const BottomElement = () => {
+
+  const [infoToggle, setInfoToggle] = useState(false);
+
+  let messageClass = infoToggle ? 'show-message' : 'hide-message';
+  let animateBtnClass = infoToggle ? 'animate-bounce' : null;
+  let BtnContent = infoToggle ? 'X' : '?';
+  
   return (
     <div className="flex-none flex p-6">
-      <button className="help-btn ml-auto bg-accent rounded-full text-2xl shadow-lg">?</button>
+      <button
+        className={`help-btn ml-auto bg-accent rounded-full text-2xl shadow-lg ${animateBtnClass}`}
+        onClick={() => setInfoToggle(!infoToggle)}>
+          {BtnContent}
+      </button>
+      <div className={`absolute right-24 bottom-24 text-4xl bg-accent text-purple-700 p-4 rounded-lg ${messageClass}`}>Don't Click a Card Twice!</div>
     </div>
   )
 }
